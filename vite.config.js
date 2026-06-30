2aec5f39e3a47fa10265bac0f2561e7ccf940409
AndreaRottini <rottiniandrea701@gmail.com>
2026-06-30 10:52:52 +0200

    Vercel fix

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-    server: {
-      port: 5173,
-      strictPort: true,
-      host: true,
-    },
-  }
-
-  if (command !== 'serve') {
-    config.base = '/SitoPortfolio/'
-  }
-
-  return config
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+  },
 })
